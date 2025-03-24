e2cd4e7b5c21f1308d04b34c0290a2b29105aa4c
Ulugbek Umaraliev <UUmaraliev@beeline.uz>
2025-03-24 16:28:12 +0500

    Add staticfiles urls

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -136,7 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "documents/"
+
 STATIC_ROOT = BASE_DIR / "static"
+MEDIA_ROOT = BASE_DIR / "documents"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- PROJECT/urls.py
+++ PROJECT/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(prefix=settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(prefix=settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
